80026e4d19afc97193060c6badc354d3f1a1bd2f
Matteo Vivona <kapa89@me.com>
2017-08-12 17:35:34 +0200

    Update install.sh
    Adds host.conf fixes

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-apt-get update
+apt-get update # To get the latest package lists
 
 apt-get install -y \
     linux-image-extra-$(uname -r) \
     linux-image-extra-virtual
 
-apt-get update
+apt-get update # To update package lists
 
 apt-get install -y \
     apt-transport-https \
@@ -20,8 +20,21 @@ add-apt-repository \
    $(lsb_release -cs) \
    stable edge"
 
-apt-get update
+apt-get update # To update package lists
 
-apt-get install -y docker-ce
+apt-get install -y docker-ce # Install Docker CE Stable
 
-docker version
+docker version # Check Docker version
+
+cat > /etc/systemd/system/docker.service.d/hosts.conf <<EOL
+[Service]
+ExecStart=
+ExecStart=/usr/bin/dockerd -H fd:// -H tcp://0.0.0.0:2376 --storage-driver aufs --tlsverify --tlscacert /etc/docker/ca.pem --tlscert /etc/docker/server.pem --tlskey /etc/docker/server-key.pem --label provider=generic 
+Environment=
+EOL
+
+systemctl daemon-reload
+
+service docker restart
+
+service docker status
