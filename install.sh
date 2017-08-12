55b47180834d53a35ce0b47625e8d85b872986fc
Matteo Vivona <kapa89@me.com>
2017-08-12 19:34:54 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,6 +30,8 @@ sudo mkdir /etc/systemd/system/docker.service.d/ # Fix docker-engine start fail
 
 sudo wget -O /etc/systemd/system/docker.service.d/hosts.conf "https://raw.githubusercontent.com/tehKapa/docker-infrastructure/master/hosts.conf" 
 
+sudo chmod +x /etc/systemd/system/docker.service.d/hosts.conf
+
 sudo systemctl daemon-reload 
 
 sudo service docker restart # Restart Docker Deamon
