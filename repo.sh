5eaee2885b19413d2b8c0cfc5627362523cedefe
Matteo Vivona <kapa89@me.com>
2017-08-18 16:08:02 +0200

    Create repo.sh

---
+++ repo.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+sudo apt-get update # To get the latest package lists
+
+sudo apt-get upgrade
+
+sudo apt-get install -y \
+    linux-image-extra-$(uname -r) \
+    linux-image-extra-virtual
+
+sudo apt-get update # To update package lists
+
+sudo apt-get autoclean
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable edge"
