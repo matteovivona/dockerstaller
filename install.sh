0d13e9b32390665d6ae8ade1e9b66427f9114176
Matteo Vivona <kapa89@me.com>
2017-09-16 12:40:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,4 @@ sudo apt-get update # To get the latest package lists
 
 sudo apt-get install -y docker-ce # Install Docker CE Stable
 
-systemctl enable docker
-
 sudo docker version # Check Docker version
