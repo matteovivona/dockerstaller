0986a8d569c5141df039121bf94a851e28b55763
Matteo Vivona <matteo.vivona@me.com>
2020-03-05 20:40:06 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -66,7 +66,7 @@ sudo systemctl enable docker
 
 sudo apt autoremove -y
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
 
